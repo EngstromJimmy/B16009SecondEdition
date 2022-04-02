e891afa26dcedaa7eb52aaf962d14521c6158261
Jimmy Engström <jimmy@apeoholic.se>
2022-04-02 17:51:14 +0200

    Initial checking

---
+++ Chapter02/MyBlog/BlazorWebAssembly/Client/Program.cs
@@ -0,0 +1,13 @@
+using BlazorWebAssembly.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+//<Chapter2 WebAssembly Program>
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
+//</Chapter2 WebAssembly Program>
