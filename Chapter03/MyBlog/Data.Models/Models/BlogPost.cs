eb2f94b9482dae4f95200809a2ba6493b3f27f8d
Jimmy Engström <jimmy@apeoholic.se>
2022-04-30 14:31:52 +0200

    Updated to .NET7 and added Tag

--- Chapter03/MyBlog/Data.Models/Models/BlogPost.cs
+++ Chapter03/MyBlog/Data.Models/Models/BlogPost.cs
@@ -1,11 +1,11 @@
 ﻿namespace MyBlog.Data.Models;
 
-public class BlogPost : IMyBlogItem
+public class BlogPost
 {
     public int Id { get; set; }
     public string Title { get; set; }
     public string Text { get; set; }
     public DateTime PublishDate { get; set; }
     public Category Category { get; set; }
-    public ICollection<Tag> Tags { get; set; }
+    public List<Tag> Tags { get; set; }
 }
--- Chapter03/MyBlog/Data.Models/Models/Tag.cs
+++ Chapter03/MyBlog/Data.Models/Models/Tag.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Data.Models.Models
+﻿namespace MyBlog.Data.Models;
+public class Tag
 {
-    public class Tag
-    {
-        
-    }
+    public int Id { get; set; }
+    public string Name { get; set; } = ""
 }
