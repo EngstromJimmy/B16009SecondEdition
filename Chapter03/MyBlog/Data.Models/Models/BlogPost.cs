8ca17b44e3fea20d4cc083d45aa2d9b165d8bcb8
Jimmy Engström <jimmy@apeoholic.se>
2022-04-30 14:36:06 +0200

    Added warnings as Error and added Tags

--- Chapter03/MyBlog/Data.Models/Models/BlogPost.cs
+++ Chapter03/MyBlog/Data.Models/Models/BlogPost.cs
@@ -3,9 +3,9 @@
 public class BlogPost
 {
     public int Id { get; set; }
-    public string Title { get; set; }
-    public string Text { get; set; }
+    public string Title { get; set; } = "";
+    public string Text { get; set; } = "";
     public DateTime PublishDate { get; set; }
-    public Category Category { get; set; }
-    public List<Tag> Tags { get; set; }
+    public Category? Category { get; set; }
+    public List<Tag> Tags { get; set; } = new();
 }
--- Chapter03/MyBlog/Data.Models/Models/Tag.cs
+++ Chapter03/MyBlog/Data.Models/Models/Tag.cs
@@ -2,5 +2,5 @@
 public class Tag
 {
     public int Id { get; set; }
-    public string Name { get; set; } = ""
+    public string Name { get; set; } = "";
 }
