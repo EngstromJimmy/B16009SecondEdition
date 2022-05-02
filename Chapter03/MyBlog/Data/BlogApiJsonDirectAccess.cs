4a9387f563eae599a92a21dd2e91b41d447d5da9
Jimmy Engström <jimmy@apeoholic.se>
2022-05-02 17:18:12 +0200

    Added JSON repo

---
+++ Chapter03/MyBlog/Data/BlogApiJsonDirectAccess.cs
@@ -0,0 +1,200 @@
+﻿using Data.Models;
+using Data.Models.Interfaces;
+using Microsoft.Extensions.Options;
+using System.Text.Json;
+
+namespace Data;
+public class BlogApiJsonDirectAccess : IBlogApi
+{
+    //<Settings>
+    BlogApiJsonDirectAccessSetting _settings;
+    public BlogApiJsonDirectAccess(IOptions<BlogApiJsonDirectAccessSetting> option)
+    {
+        _settings = option.Value;
+    }
+    //</Settings>
+
+    //<Private variables>
+    private List<BlogPost>? _blogPosts;
+    private List<Category>? _categories;
+    private List<Tag>? _tags;
+    //</Private variables>
+
+    //<LoadData>
+    private void Load<T>(ref List<T>? list, string folder)
+    {
+        if (list == null)
+        {
+            list = new();
+            var fullpath = $@"{_settings.DataPath}\{folder}";
+            if (!Directory.Exists(fullpath))
+            {
+                Directory.CreateDirectory(fullpath);
+            }
+            foreach (var f in Directory.GetFiles(fullpath))
+            {
+                var json = File.ReadAllText(f);
+                var bp = JsonSerializer.Deserialize<T>(json);
+                if (bp != null)
+                {
+                    list.Add(bp);
+                }
+            }
+        }
+    }
+    private Task LoadBlogPostsAsync()
+    {
+        Load<BlogPost>(ref _blogPosts, _settings.BlogPostsFolder);
+        return Task.CompletedTask;
+    }
+    private Task LoadTagsAsync()
+    {
+        Load<Tag>(ref _tags, _settings.TagsFolder);
+        return Task.CompletedTask;
+    }
+    private Task LoadCategoriesAsync()
+    {
+        Load<Category>(ref _categories, _settings.CategoriesFolder);
+        return Task.CompletedTask;
+    }
+    //</LoadData>
+
+    //<ManipulateData>
+    private async Task Save<T>(List<T>? list, string folder, string filename, T item)
+    {
+        var filepath = $@"{_settings.DataPath}\{folder}\{filename}";
+        await File.WriteAllTextAsync(filepath, JsonSerializer.Serialize<T>(item));
+        if (list == null)
+        {
+            list = new();
+        }
+        list.Add(item);
+    }
+    private void Delete<T>(List<T>? list, string folder, string filename, T item)
+    {
+        var filepath = $@"{_settings.DataPath}\{folder}\{filename}";
+        try
+        {
+            File.Delete(filepath);
+            if (list != null)
+            {
+                list.Remove(item);
+            }
+        }
+        catch { }
+    }
+    //<ManipulateData>
+    //<GetBlogPosts>
+    public async Task<List<BlogPost>> GetBlogPostsAsync(int numberofposts, int startindex)
+    {
+        await LoadBlogPostsAsync();
+        return _blogPosts ?? new();
+    }
+
+    public async Task<BlogPost> GetBlogPostAsync(string id)
+    {
+        await LoadBlogPostsAsync();
+        if (_blogPosts == null)
+            throw new Exception("Blog posts not found");
+        return _blogPosts.First(b => b.Id == id);
+    }
+
+    public async Task<int> GetBlogPostCountAsync()
+    {
+        await LoadBlogPostsAsync();
+        if (_blogPosts == null)
+            return 0;
+        else
+            return _blogPosts.Count();
+    }
+    //</GetBlogPosts>
+    //<GetCategories>
+    public async Task<List<Category>> GetCategoriesAsync()
+    {
+        await LoadCategoriesAsync();
+        return _categories ?? new();
+    }
+
+    public async Task<Category> GetCategoryAsync(string id)
+    {
+        await LoadCategoriesAsync();
+        if (_categories == null)
+            throw new Exception("Categories not found");
+        return _categories.First(b => b.Id == id);
+    }
+    //<GetCategories>
+    //<GetTags>
+    public async Task<List<Tag>> GetTagsAsync()
+    {
+        await LoadTagsAsync();
+        return _tags ?? new();
+    }
+
+    public async Task<Tag> GetTagAsync(string id)
+    {
+        await LoadTagsAsync();
+        if (_tags == null)
+            throw new Exception("Tags not found");
+        return _tags.First(b => b.Id == id);
+    }
+    //<GetTags>
+    //<Save>
+    public async Task<BlogPost> SaveBlogPostAsync(BlogPost item)
+    {
+        if (item.Id == null)
+        {
+            item.Id = Guid.NewGuid().ToString();
+        }
+        await Save<BlogPost>(_blogPosts, _settings.BlogPostsFolder, $"{item.Id}.json", item);
+        return item;
+    }
+
+    public async Task<Category> SaveCategoryAsync(Category item)
+    {
+        if (item.Id == null)
+        {
+            item.Id = Guid.NewGuid().ToString();
+        }
+        await Save<Category>(_categories, _settings.CategoriesFolder, $"{item.Id}.json", item);
+        return item;
+    }
+
+    public async Task<Tag> SaveTagAsync(Tag item)
+    {
+        if (item.Id == null)
+        {
+            item.Id = Guid.NewGuid().ToString();
+        }
+        await Save<Tag>(_tags, _settings.TagsFolder, $"{item.Id}.json", item);
+        return item;
+    }
+    //</Save>
+    //<Delete>
+    public Task DeleteBlogPostAsync(BlogPost item)
+    {
+        Delete(_blogPosts, _settings.BlogPostsFolder, $"{item}.json", item);
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteCategoryAsync(Category item)
+    {
+        Delete(_categories, _settings.CategoriesFolder, $"{item}.json", item);
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteTagAsync(Tag item)
+    {
+        Delete(_tags, _settings.TagsFolder, $"{item}.json", item);
+        return Task.CompletedTask;
+    }
+    //</Delete>
+    //<Cache>
+    public Task InvalidateCache()
+    {
+        _blogPosts = null;
+        _tags = null;
+        _categories = null;
+        return Task.CompletedTask;
+    }
+    //</Cache>
+}
